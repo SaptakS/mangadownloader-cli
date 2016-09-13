9dde0c1f05c9e5e763c12d3d159b6627a3af5a0b
Saptak Sengupta <saptak013@gmail.com>
2016-09-13 06:31:17 +0530

    Modified next link

--- mangaScrape.py
+++ mangaScrape.py
@@ -8,14 +8,12 @@ from PIL import Image
 from StringIO import StringIO
 from requests.exceptions import ConnectionError
 
-http_proxy  = "http://<username>:<password>@<host>:<port>"
-https_proxy = "http://<username>:<password>@<host>:<port>"
-ftp_proxy   = "http://<username>:<password>@<host>:<port>"
 
+proxy = "http://<username>:<password>@<host>:<port>"
 proxyDict = {
-              "http": http_proxy,
-              "https": https_proxy,
-              "ftp": ftp_proxy
+              "http": proxy,
+              "https": proxy,
+              "ftp": proxy
             }
 
 proxy_support = urllib2.ProxyHandler(proxyDict)
@@ -28,20 +26,30 @@ hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML,
        'Accept-Language': 'en-US,en;q=0.8',
        'Connection': 'keep-alive'}
 
+
 def findNextURL(imageUrl):
 
     return imageUrl.parent["href"]
+
 
 def saveImg(data):
     maxval = 0
     maxurl = ""
     imglinks = data.findAll("img")
-    image=""
+    check_link = BASE_HOST.split("//")[1]
+    if len(check_link.split('.')) > 2:
+        check_link = check_link.split('.')[1] + "." + check_link.split('.')[2]
     for link in imglinks:
-        if BASE_HOST.split("//")[1].split("www.")[1] in str(link['src']) and sm(None, str(link['src']), BASE_URL.replace("http","")).ratio() > maxval:
-            maxval = sm(None, str(link['src']),BASE_URL).ratio()
+        if check_link in str(link['src']) and sm(None, str(link['src']), BASE_URL.replace("http", "")).ratio() > maxval:
+            maxval = sm(None, str(link['src']), BASE_URL).ratio()
             maxl = link
             maxurl = link['src']
+    if maxurl == '':
+        for link in imglinks:
+            if sm(None, str(link['src']), BASE_URL.replace("http", "")).ratio() > maxval:
+                maxval = sm(None, str(link['src']), BASE_URL).ratio()
+                maxl = link
+                maxurl = link['src']
 
     if "http" not in str(maxurl):
         maxurl = BASE_HOST.split("//")[0] + maxurl
@@ -51,6 +59,7 @@ def saveImg(data):
         print "Images Saved Successfully"
     except:
         print "Error        "
+        exit(0)
 
     file = open(os.path.join(BASE_PATH, "%s.jpg") % IMAGE_TITLE, 'wb')
     try:
@@ -63,12 +72,14 @@ def saveImg(data):
 
     return maxl
 
+
 def linkData(url):
     try:
-        req = urllib2.Request(BASE_URL, headers=hdr)
-        resp = urllib2.urlopen(req).read()
+        #req = urllib2.Request(BASE_URL, headers=hdr)
+        #resp = urllib2.urlopen(req).read()
+        r = requests.get(BASE_URL, proxies=proxyDict)
 
-        data = bs("".join(resp))
+        data = bs("".join(r.text))
 
         return data
     except urllib2.HTTPError, e:
@@ -77,8 +88,9 @@ def linkData(url):
 
 BASE_PATH = raw_input("Enter Name of the Folder to save in:")
 BASE_URL = raw_input("Enter the First Page URL:")
-BASE_HOST = re.split(r"/",BASE_URL)[0] + "//" + re.split(r"/",BASE_URL)[2]
-print BASE_HOST
+BASE_HOST = re.split(r"/", BASE_URL)[0] + "//" + re.split(r"/", BASE_URL)[2]
+BASE_CHECK_LINK = BASE_URL
+#print BASE_HOST
 TOTAL_IMG = int(raw_input("Enter total images to download:"))
 IMAGE_TITLE = 0
 
@@ -91,14 +103,14 @@ imageUrl = saveImg(linkData(BASE_URL))
 ctr = 0
 while ctr < TOTAL_IMG - 1:
     IMAGE_TITLE = IMAGE_TITLE + 1
-    BASE_URL = findNextURL(imageUrl)
-    #print BASE_URL
-    if not "://" in BASE_URL:
-        BASE_URL = BASE_HOST + BASE_URL
+    next_rel = findNextURL(imageUrl)
+    #print next_rel
+    if not "://" in next_rel:
+        if next_rel.startswith("/"):
+            BASE_URL = BASE_HOST + next_rel
+        else:
+            BASE_URL = re.sub(r"\w+.html", next_rel, BASE_URL)
     print "Next page is", BASE_URL
     imageUrl = saveImg(linkData(BASE_URL))
     ctr = ctr + 1
-
-
-
-
+print "Total Pages Downloaded: ", (ctr + 1)
